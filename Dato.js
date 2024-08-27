47ab79fe3c6453020c707c9e0ed81705e8626edb
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-08-27 15:06:14 -0500

    Creation of parent Dato, and child classes of entry and exit.

---
+++ Dato.js
@@ -0,0 +1,21 @@
+class Dato{
+    constructor(descripcion, valor) { 
+        this.descripcion = descripcion;
+        this.valor
+    }
+    getDescripcion(){
+        return this.descripcion;
+    }
+    getValor(){
+        return this.valor;
+    }
+
+    setDescripcion(descripcion){
+        this.descripcion = descripcion;
+    }
+
+    setValor(valor){
+        this.valor = valor;
+    }
+
+}
